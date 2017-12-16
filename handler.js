188f70bcb5a18850467598df6fb752840c235138
Ben Smith <ben@10consulting.com>
2017-12-16 18:11:09 +0000

    Render chart from JSON stored in S3

--- handler.js
+++ handler.js
@@ -3,48 +3,46 @@
 const S3 = require('aws-sdk/clients/s3');
 const spawn = require('child_process').exec;
 
-module.exports.renderChart = function(event, context) {
+function renderChart(chartOptions, callback) {
   const ps = spawn(
-    `node node_modules/node-googlecharts/bin/node-googlecharts '${JSON.stringify(event.chartOptions)}' 'svg'`,
+    `node node_modules/node-googlecharts/bin/node-googlecharts '${chartOptions}' 'svg'`,
     (error, stdout, stderr) => {
       if (error) {
-        context.done(new Error('[ServerError] ' + error.message));
+        callback(new Error('[ServerError] ' + error.message));
       } else if (stderr) {
-        context.done(new Error('[ServerError] ' + stderr));
+        callback(new Error('[ServerError] ' + stderr));
       } else {
-        context.done(null, stdout);
+        callback(null, stdout);
       }
     }
   );
+}
+
+module.exports.renderChart = function(event, context) {
+  const chartOptions = JSON.stringify(event.chartOptions);
+
+  renderChart(chartOptions, context.done);
 };
 
 module.exports.renderS3Chart = function(event, context) {
-  console.log('event', event);
-
   const s3Bucket = event.s3Bucket;
   const chart = event.chart;
   const data = event.data;
 
-  console.log('s3Bucket', s3Bucket);
-  console.log('chart', chart);
-  console.log('data', data);
-
   const s3 = new S3({
     region: 'eu-west-2'
   });
-
-  console.log('Getting from S3', s3Bucket, `${chart}.json`);
 
   s3.getObject({
     Bucket: s3Bucket,
     Key: `${chart}.json`
   }, function(err, data) {
     if (err) {
-      console.log('s3 error', err);
       context.done(new Error(err));
     }
 
-    console.log('s3 ok', data);
-    context.done(null, data);
+    const content = data.Body.toString('utf-8');
+
+    renderChart(content, context.done);
   });
 }
