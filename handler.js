41a51e461102e04c5ba57d224c2f83593046670f
Ben Smith <ben@10consulting.com>
2017-12-18 14:16:54 +0000

    Allow chart options read from S3 to include substituted variables
    A variable, such as `${foo}`, can be replaced by providing a URL param of the same key, e.g `/chart?foo=bar`.

--- handler.js
+++ handler.js
@@ -1,33 +1,40 @@
 'use strict';
 
 const S3 = require('aws-sdk/clients/s3');
-const spawn = require('child_process').exec;
+const nodeGoogleCharts = require('@slashdotdash/node-googlecharts');
 
+// Render a Google JavaScript chart using the provided `chartOptions`.
+// A standard `callback` function must be provided to return the generated chart
+// SVG or any error.
 function renderChart(chartOptions, callback) {
-  const ps = spawn(
-    `node node_modules/node-googlecharts/bin/node-googlecharts '${chartOptions}' 'svg'`,
-    (error, stdout, stderr) => {
-      if (error) {
-        callback(new Error('[ServerError] ' + error.message));
-      } else if (stderr) {
-        callback(new Error('[ServerError] ' + stderr));
-      } else {
-        callback(null, stdout);
-      }
-    }
-  );
+  nodeGoogleCharts(chartOptions, 'svg')
+    .then(chart => callback(null, chart))
+    .catch(err => callback(err));
 }
 
+// Replace all occurences of templated params (e.g. `${foo}`) with their
+// actual value from the given replacements object (e.g. `{foo: 'bar'}`).
+function replaceValues(chartOpts, replacements) {
+  return Object.keys(replacements)
+    .reduce((acc, key) => {
+      return acc.replace('${' + key + '}', replacements[key]);
+    }, chartOpts);
+}
+
+// Render a Google JavaScript chart using chart options provided by the `event`
+// arg.
 module.exports.renderChart = function(event, context) {
-  const chartOptions = JSON.stringify(event.chartOptions);
+  const chartOptions = event.chartOptions;
 
   renderChart(chartOptions, context.done);
 };
 
+// Render a Google JavaScript chart where the chart options are stored as JSON
+// in a file hosted in an S3 bucket.
 module.exports.renderS3Chart = function(event, context) {
   const s3Bucket = event.s3Bucket;
   const chart = event.chart;
-  const data = event.data;
+  const chartData = event.data || {};
 
   const s3 = new S3({
     region: 'eu-west-2'
@@ -39,10 +46,13 @@ module.exports.renderS3Chart = function(event, context) {
   }, function(err, data) {
     if (err) {
       context.done(new Error(err));
+      return;
     }
 
     const content = data.Body.toString('utf-8');
+    const substituted = replaceValues(content, chartData);
+    const chartOptions = JSON.parse(substituted);
 
-    renderChart(content, context.done);
+    renderChart(chartOptions, context.done);
   });
 }
